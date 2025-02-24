2f3cbde567e70e17c88a54bdd5c5cc0977912b7c
mansur0095 <mansur.ibragimov@stud.fh-campuswien.ac.at>
2025-02-24 23:36:52 +0100

    created more testcases

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -32,4 +32,51 @@ class HomeControllerTest {
         assertEquals(moviesExpected,movies);
     }
 
+    @Test
+    void filter_by_string_matches_with_actual_output()
+    {
+        //given
+        homeController.initializeObserverable();
+        List<Movie> movies = homeController.filterByString("Joker",homeController.observableMovies);
+
+        //when
+        List<Movie> moviesExpected = Arrays.asList(
+                new Movie(
+                        "Joker",
+                        "A mentally troubled comedian, Arthur Fleck, embarks on a downward spiral of revolution and bloody crime in Gotham City, leading to his alter ego: the Joker.",
+                        Arrays.asList(Genre.DRAMA, Genre.THRILLER, Genre.CRIME)),
+                new Movie(
+                        "The Dark Knight",
+                        "Batman faces his greatest challenge when the Joker wreaks havoc on Gotham, testing the hero's limits and morality.",
+                        Arrays.asList(Genre.ACTION, Genre.CRIME, Genre.DRAMA))
+        );
+
+        //then
+        assertEquals(moviesExpected,movies);
+    }
+
+    @Test
+    void filter_by_genre_matches_with_actual_output()
+    {
+        //given
+        homeController.initializeObserverable();
+        List<Movie> movies = homeController.filterByGenre(Genre.ROMANCE,homeController.observableMovies);
+
+        //when
+
+        List<Movie> moviesExpected = new ArrayList<>();
+        moviesExpected.add(new Movie(
+                "Titanic",
+                "A young aristocrat falls in love with a kind but poor artist aboard the ill-fated Titanic, as the ship meets its tragic destiny.",
+                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.HISTORY)));
+        //then
+        assertEquals(moviesExpected,movies);
+    }
+
+    @Test
+    void at_initialization() {
+        homeController.initializeObserverable();
+        assertEquals(homeController.allMovies, homeController.observableMovies);
+    }
+
 }
